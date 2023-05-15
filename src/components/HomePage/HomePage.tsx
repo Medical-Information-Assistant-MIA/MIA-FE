6a1990a9bd4ef1b11e0505118b6ad5fc18562e29
Sophie LaBelle <sophie.r.labelle@gmail.com>
2023-05-15 16:15:48 -0700

    Add basic positioning and styling to the nav component

--- src/components/HomePage/HomePage.tsx
+++ src/components/HomePage/HomePage.tsx
@@ -2,7 +2,8 @@ import './HomePage.css'
 
 export const HomePage = () => {
   return (
-    <>
-    </>
+    <section className='home-page'>
+
+    </section>
   )
 }
--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -2,8 +2,9 @@ import './NavBar.css'
 
 export const NavBar =  () => {
   return (
-    <>
+    <nav className='nav-bar'>
       <h1>Mia</h1>
-    </>
+      <button>Login</button>
+    </nav>
   )
 }
