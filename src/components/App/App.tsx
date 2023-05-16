d49b7d6025980f508b0e666189496f2914cbae86
Kass Daniello <zertroz@gmail.com>
2023-05-16 12:12:53 -0400

    Create Condition page and setup routes
    Co-authored-by: Amber Shipley <ashipley2@me.com>
    Co-authored-by: Sophie Labelle <sophie.r.labelle@gmail.com>

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -2,13 +2,18 @@ import { UserDashboard } from '../UserDashboard/UserDashboard';
 import { HomePage } from '../HomePage/HomePage';
 import { NavBar } from '../NavBar/NavBar';
 import './App.css';
+import { ConditionPage } from '../ConditionPage/ConditionPage';
+import { Route, Switch } from 'react-router-dom';
 
 export const App = () => {
   return (
   <main>
     <NavBar />
-    <HomePage />
-    <UserDashboard />
+    <Switch>
+      <Route exact path='/' render={() => <HomePage />} />
+      <Route exact path='/user-dashboard' render={() => <UserDashboard />} />
+      <Route exact path='/conditions/:condition' render={({match}) => <ConditionPage key={match.params.condition}/>} />
+    </Switch>
   </main>
   )
 }
