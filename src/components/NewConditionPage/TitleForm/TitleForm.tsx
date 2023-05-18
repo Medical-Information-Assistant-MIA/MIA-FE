6b65a98df8ebc3f8db7009eeb4b0ffcfbac2a652
Amber Shipley <ashipley2@me.com>
2023-05-18 16:34:14 -0700

    Create our first mutation on creating a new conditiongit add .

--- src/components/NewConditionPage/TitleForm/TitleForm.tsx
+++ src/components/NewConditionPage/TitleForm/TitleForm.tsx
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import { useMutation, gql } from '@apollo/client';
 import './TitleForm.css';
 
 
@@ -9,6 +10,23 @@ type TitleFormProps = {
 export const TitleForm = ({ userId }: TitleFormProps) => {
   const [conditionName, setConditionName] = useState('');
 
+  const CREATE_CONDITION = gql`
+  mutation {
+    createCondition(input: {
+      name: "${conditionName}"
+      userId: ${userId}
+    }) {
+      condition {
+        id
+        name 
+      }
+    }
+  }
+  `
+  const [mutateFunction, { data, loading, error }] = useMutation(CREATE_CONDITION)
+  if (loading) console.log(loading, 'loading')
+  if (error) console.log(error, 'error')
+  console.log(data)
 
   return (
     <section className='condition-form nav-spacing'>
@@ -17,7 +35,7 @@ export const TitleForm = ({ userId }: TitleFormProps) => {
         e.preventDefault()
         console.log(conditionName)
         setConditionName('')
-        
+        mutateFunction()
       }}>
         <label className='med-label'>
           What is the name of your Condition?
