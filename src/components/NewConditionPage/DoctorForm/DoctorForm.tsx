c7da7329f7111db2708d1633c7cb8bb90c75fcd8
Amber Shipley <ashipley2@me.com>
2023-05-18 14:17:54 -0700

    Create form for adding a doctor

--- src/components/NewConditionPage/DoctorForm/DoctorForm.tsx
+++ src/components/NewConditionPage/DoctorForm/DoctorForm.tsx
@@ -1,7 +1,61 @@
 import './DoctorForm.css';
+import { useState } from 'react';
 
 export const DoctorForm = () => {
+  const [name, setName] = useState('');
+  const [phone, setPhone] = useState('');
+  const [address, setAddress] = useState('');
+  const [specialty, setSpecialty] = useState('');
+
+  
   return (
-    <p></p>
+    <section className='add-doctor'>
+      <h2>Add a Doctor</h2>
+      <form>
+          <div>
+          <label>
+            Add your doctor's name:
+            <input 
+              type='text'
+              value={name}
+              name='doctors name'
+              placeholder='name'
+              onChange={e => setName(e.target.value)}
+              />
+          </label>
+          <label>
+            Add your doctor's phone number:
+            <input 
+              type='text'
+              value={phone}
+              name='doctors phone number'
+              placeholder='phone number'
+              onChange={e => setPhone(e.target.value)}
+              />
+          </label>
+          <label>
+            Add your doctor's address here:
+            <input 
+              type='text'
+              value={address}
+              name='doctors address'
+              placeholder='address'
+              onChange={e => setAddress(e.target.value)}
+              />
+          </label>
+          <label>
+            Add your doctor's specialty:
+            <input 
+              type='text'
+              value={specialty}
+              name='doctors specialty'
+              placeholder='specialty'
+              onChange={e => setSpecialty(e.target.value)}
+              />
+          </label>
+        </div>
+        <button className='submit-button' type='submit'>Go to health events</button>
+      </form>
+    </section>
   )
 }
