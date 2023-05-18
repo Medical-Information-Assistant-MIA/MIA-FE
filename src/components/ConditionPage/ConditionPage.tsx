53ba52e8abc97a5d051449e22a5555c4f921b000
Sophie LaBelle <sophie.r.labelle@gmail.com>
2023-05-18 10:49:19 -0700

    Add doctor and health events display, types and styling
    Co-authored-by: Amber Shipley <ashipley2@me.com>
    Co-authored-by: Kass Daniello <zertroz@gmail.com>

--- src/components/ConditionPage/ConditionPage.tsx
+++ src/components/ConditionPage/ConditionPage.tsx
@@ -1,9 +1,30 @@
 import './ConditionPage.css'
-import { useRouteMatch } from 'react-router-dom';
+import { useRouteMatch, Link } from 'react-router-dom';
 import { useQuery, gql } from '@apollo/client';
 
 type MatchParams = {
   id: string;
+}
+
+type Medication = { 
+  name: string,
+  datePrescribed: string,
+  dosage: string,
+  frequency: string,
+  prescribedBy: string
+}
+
+type Doctor = {
+  address: string,
+  category: string,
+  name: string,
+  phone: string
+}
+
+type HealthEvent = {
+  date: string,
+  category: string,
+  note: string
 }
 
 export const ConditionPage = () => {
@@ -41,17 +62,12 @@ export const ConditionPage = () => {
   if (error) return <p>{error.message}</p>
   if(data) {
     const {name, medications, doctors, healthEvents} = data.condition
-    console.log('condition query', medications)
+    console.log('condition query', healthEvents)
 
     const medDisplay = medications.length ? 
-      medications.map((med: { name: string,
-      datePrescribed: string,
-      dosage: string,
-      frequency: string,
-      prescribedBy: string
-    })  =>{
+      medications.map((med: Medication) => {
       return (
-        <div>
+        <div className='medication'>
           <p>Medication Name: {med.name}</p>
           <p>Date Prescribed: {med.datePrescribed}</p>
           <p>Dosage: {med.dosage}</p>
@@ -59,7 +75,30 @@ export const ConditionPage = () => {
           <p>Prescribed By: {med.prescribedBy}</p>
         </div>
       )
-    }) : <p>No Meds Added</p>
+    }) : <p>No Medications Added</p>
+
+    const docDisplay = doctors.length ? 
+      doctors.map((doc: Doctor) => {
+      return (
+        <div className='doctor'>
+          <p>{doc.name}</p>
+          <p>{doc.category}</p>
+          <p>{doc.address}</p>
+          <p>{doc.phone}</p>
+        </div>
+      )
+    }) : <p>No Doctors Added</p>
+
+    const healthEventDisplay = healthEvents.length ? 
+      healthEvents.map((event: HealthEvent) => {
+      return (
+        <div className='health-event'>
+          <p>{event.date}</p>
+          <p>{event.category}</p>
+          <p>{event.note}</p>
+        </div>
+      )
+    }) : <p>No Notes Added</p>
 
     return (
       <section className='condition-page nav-spacing'>
@@ -67,11 +106,14 @@ export const ConditionPage = () => {
         <h3>Medications</h3>
         {medDisplay}
         <h3>Doctors</h3>
-        <p>blah</p>
-        <h3>Health Events</h3>
-        <div 
-          className='site-description'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        <div className='all-doctors'>
+          {docDisplay}
         </div>
+        <h3>Health Events</h3>
+        {healthEventDisplay}
+        <Link to='/user-dashboard'>
+          <button>To DashBoard</button>
+        </Link>
       </section>
     )
   }
