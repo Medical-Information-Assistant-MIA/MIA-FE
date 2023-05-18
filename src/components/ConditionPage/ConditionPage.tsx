4777c694587873c7afbd9919fc7053e65f2e23bb
Sophie LaBelle <sophie.r.labelle@gmail.com>
2023-05-18 10:29:58 -0700

    Add display of medications
    Co-authored-by: Kass Daniello <zertroz@gmail.com>
    Co-authored-by: Amber Shipley <ashipley2@me.com>

--- src/components/ConditionPage/ConditionPage.tsx
+++ src/components/ConditionPage/ConditionPage.tsx
@@ -35,22 +35,47 @@ export const ConditionPage = () => {
       }
     }
   `
-  const { loading, error, data } = useQuery(GET_CONDITION);
-  if (loading) console.log(loading, 'loading')
-  if (error) console.log(error.message, 'error')
-  console.log('condition query', data)
 
+  const { loading, error, data } = useQuery(GET_CONDITION);
+  if (loading) return <p>Loading...</p>
+  if (error) return <p>{error.message}</p>
+  if(data) {
+    const {name, medications, doctors, healthEvents} = data.condition
+    console.log('condition query', medications)
+
+    const medDisplay = medications.length ? 
+      medications.map((med: { name: string,
+      datePrescribed: string,
+      dosage: string,
+      frequency: string,
+      prescribedBy: string
+    })  =>{
+      return (
+        <div>
+          <p>Medication Name: {med.name}</p>
+          <p>Date Prescribed: {med.datePrescribed}</p>
+          <p>Dosage: {med.dosage}</p>
+          <p>Frequency: {med.prescribedBy}</p>
+          <p>Prescribed By: {med.prescribedBy}</p>
+        </div>
+      )
+    }) : <p>No Meds Added</p>
+
+    return (
+      <section className='condition-page nav-spacing'>
+        <h2>{name}</h2>
+        <h3>Medications</h3>
+        {medDisplay}
+        <h3>Doctors</h3>
+        <p>blah</p>
+        <h3>Health Events</h3>
+        <div 
+          className='site-description'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        </div>
+      </section>
+    )
+  }
   return (
-    <section className='condition-page nav-spacing'>
-      <h2>Condition title</h2>
-      <h3>Medications</h3>
-      <p>Your meds here</p>
-      <h3>Doctors</h3>
-      <p>Your docs here</p>
-      <h3>Health Events</h3>
-      <div 
-        className='site-description'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-      </div>
-    </section>
+    <></>
   )
 }
