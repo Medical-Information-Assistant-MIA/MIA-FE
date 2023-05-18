4f118e8b2551aac52b94682db442e7b421101c51
Sophie LaBelle <sophie.r.labelle@gmail.com>
2023-05-18 14:57:46 -0700

    Create title form, css and state

--- src/components/NewConditionPage/TitleForm/TitleForm.tsx
+++ src/components/NewConditionPage/TitleForm/TitleForm.tsx
@@ -1,7 +1,27 @@
-import './TitleForm.css'
+import { useState } from 'react';
+import './TitleForm.css';
 
 export const TitleForm = () => {
+  const [conditionName, setConditionName] = useState('');
+
   return (
-    <p></p>
+    <section className='condition-form nav-spacing'>
+      <h3> Add Your Condition</h3>
+      <form className='condition-form' onSubmit={e => {
+        e.preventDefault()
+        console.log(conditionName)
+      }}>
+        <label className='med-label'>
+          What is the name of your Condition?
+          <input 
+            type='text'
+            value={conditionName} 
+            name='condition name' 
+            placeholder='Condition Name'
+            onChange={e => setConditionName(e.target.value)}/>
+        </label>
+        <button className='submit-button' >Submit condition</button>
+      </form>
+    </section>
   )
 }
