9a37d74608b5c3e0618061d43326211375b68aac
Amber Shipley <ashipley2@me.com>
2023-05-19 10:59:21 -0700

    Implement Switch in ConditionPage

--- src/components/NewConditionPage/NewConditionPage.tsx
+++ src/components/NewConditionPage/NewConditionPage.tsx
@@ -2,6 +2,7 @@ import { DoctorForm } from './DoctorForm/DoctorForm'
 import { HealthEventForm } from './HealthEventForm/HealthEventForm'
 import { MedicationForm } from './MedicationForm/MedicationForm'
 import './NewConditionPage.css'
+import { Route, Switch } from 'react-router-dom'
 import { TitleForm } from './TitleForm/TitleForm'
 
 type NewConditionProps = {
@@ -13,10 +14,12 @@ export const NewConditionPage = ({ userId, setConditionId }: NewConditionProps) 
   return (
     <section className='nav-spacing'>
       <h2>Create a New Condition</h2>
-      <TitleForm userId={userId} setConditionId={setConditionId}/>
-      <MedicationForm />
-      <DoctorForm />
-      <HealthEventForm />
+      <Switch>
+        <Route exact path='/add-condition' render={() => <TitleForm userId={userId} setConditionId={setConditionId}/>}/>
+        <Route exact path='/add-condition/add-medication' render={() => <MedicationForm />}/>
+        <Route exact path='/add-condition/add-doctor' render={() => <DoctorForm />}/>
+        {/* <HealthEventForm /> */}
+      </Switch>
     </section>
   )
 }
