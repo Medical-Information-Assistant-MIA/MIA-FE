00f5bba7c51e75b002db6ba59e0a12c84be615cc
Sophie LaBelle <sophie.r.labelle@gmail.com>
2023-05-19 16:03:09 -0700

    Add mutation function to health event form and submit functionality
    Co-authored-by: Kass Daniello <zertroz@gmail.com>

--- src/components/NewConditionPage/HealthEventForm/HealthEventForm.tsx
+++ src/components/NewConditionPage/HealthEventForm/HealthEventForm.tsx
@@ -1,4 +1,6 @@
 import { useState } from 'react'
+import { useHistory } from 'react-router-dom';
+import { useMutation, gql } from '@apollo/client';
 import './HealthEventForm.css'
 
 type NewEventProps = {
@@ -6,32 +8,87 @@ type NewEventProps = {
 }
 
 export const HealthEventForm = ({conditionId}: NewEventProps) => {
+  const history = useHistory();
   const [eventObj, setEventObj] = useState({
-    type: '',
+    category: '',
     date: '',
     note: ''
   })
 
+  const goToDashBoard = () => {
+    // if(!Object.values(medObj).length) {
+      history.push('/user-dashboard');
+    // }
+  }
+
+  const CREATE_NOTE = gql `
+    mutation {
+      createHealthEvent(input: {
+        conditionId: ${conditionId},
+        note: "${eventObj.note}",
+        date: "${eventObj.date}",
+        category: "${eventObj.category}"
+      }) {
+        healthEvent {
+          id
+          note
+          date
+          category
+        }
+        errors
+      }
+    }
+  `
+
+  const [mutateFunction, {data, loading, error}] = useMutation(CREATE_NOTE);
+  if (loading) return <p>Loading...</p>
+  if (error) return <p>Error: {error.message}</p>
+  console.log(eventObj)
+  console.log('data for HE', data)
+
   return (
     <section>
-      <form onSubmit={e => {
+      <form onSubmit={async e => {
         e.preventDefault()
-        console.log(eventObj)
+        await mutateFunction();
+        setEventObj({
+          category: '',
+          date: '',
+          note: ''
+        })
       }}>
-        <label>Select the event type:</label>
         <h2>Create Event Notes</h2>
-        <select value={eventObj.type} name='type' onChange={e => setEventObj({...eventObj, [e.target.name]: e.target.value })}>
-          <option value='symptom'>Symptom</option>
-          <option value='doctor-visit'>Doctor's Visit</option>
-          <option value='other-note'>Other Note</option>
-        </select>
-        <label>Select date:</label>
-        <input value={eventObj.date} type='date' name='date' onChange={e => setEventObj({...eventObj, [e.target.name]: e.target.value })} />
-        <label>Describe the event:</label>
-        <input value={eventObj.note} type='input' name='note' onChange={e => setEventObj({...eventObj, [e.target.name]: e.target.value })} />
+        <label>
+          Select the event type:
+          <select 
+            value={eventObj.category} 
+            name='category' 
+            onChange={e => setEventObj({...eventObj, [e.target.name]: e.target.value })}>
+            <option defaultValue='' hidden>Select health event type:</option>
+            <option value='symptom'>Symptom</option>
+            <option value='doctor-visit'>Doctor's Visit</option>
+            <option value='other-note'>Other Note</option>
+          </select>
+        </label>
+        <label>
+          Select date:
+          <input 
+            type='date' 
+            value={eventObj.date} 
+            name='date' 
+            onChange={e => setEventObj({...eventObj, [e.target.name]: e.target.value })} />
+        </label>
+        <label>
+          Describe the event:
+          <input 
+            value={eventObj.note} 
+            type='input' 
+            name='note' 
+            onChange={e => setEventObj({...eventObj, [e.target.name]: e.target.value })} />
+        </label>
         <button>Add New Note</button>
-        <button>Submit Condition</button>
       </form>
+      <button onClick={() => goToDashBoard()}>Finish and Return to Dash</button>
     </section>
   )
 }
