8ceb98f893740cd6e2aad74117d0386ecf96efb3
Kass Daniello <zertroz@gmail.com>
2023-05-20 16:42:02 -0400

    Create rudimentary verification

--- src/components/LoginPage/LoginPage.tsx
+++ src/components/LoginPage/LoginPage.tsx
@@ -9,19 +9,29 @@ type UserID = {
 export const LoginPage = ({setUserId}: UserID) =>  {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
+  const [error, setError] = useState(false)
   const history = useHistory()
+
+  const checkLogin= () => {
+    if((username === '1' || username === '2') && password === 'mia123') {
+      setUserId(username)
+      history.push('/user-dashboard')
+    } else {
+      setError(true)
+    }
+  }
 
   return (
     <section className='nav-spacing login'>
       <form onSubmit={ e => {
         e.preventDefault()
-        setUserId(username)
-        history.push('/user-dashboard')
+        checkLogin()
       }}>
-        <input type='text' value={username} name='username' placeholder='username' onChange={(e) => setUsername(e.target.value)} />
-        <input type='text' value={password} name='password' placeholder='password' onChange={(e) => setPassword(e.target.value)} />
+        <input type='text' value={username} name='username' placeholder='username' required onChange={(e) => setUsername(e.target.value)} />
+        <input type='password' value={password} name='password' placeholder='password' required onChange={(e) => setPassword(e.target.value)} />
         <button type='submit'>Login</button>
       </form>
+      {error ? <p>Incorrect login, please try again.</p> : null}
     </section>
   )
 }
