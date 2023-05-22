4908f6fc45bccdb1181c8f322c4400bec79ce9d7
Sophie LaBelle <sophie.r.labelle@gmail.com>
2023-05-22 13:05:33 -0700

    Update error handling for logo route

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -6,7 +6,7 @@ export const NavBar =  () => {
   const location = useLocation();
   const showLogin = location.pathname === '/' ? <button className='nav-btn go-back-btn'>Login</button> : <button className='nav-btn go-back-btn hidden'>Login</button>
   const showLogOut = location.pathname !== '/' ? <button className='nav-btn go-back-btn'>Logout</button> : <button className='nav-btn go-back-btn hidden'>Logout</button>
-  const logoRoute = location.pathname !== '/' ? <Link to='/user-dashboard'><img className='logo' src={require('../../Mia-logo.png')} alt='Mia: Medical Information Assistant' /></Link> : <Link to='/'><img className='logo' src={require('../../Mia-logo.png')} alt='Mia: Medical Information Assistant' /></Link>
+  const logoRoute = location.pathname !== '/login' && location.pathname !== '/' ? <Link to='/user-dashboard'><img className='logo' src={require('../../Mia-logo.png')} alt='Mia: Medical Information Assistant' /></Link> : <Link to='/'><img className='logo' src={require('../../Mia-logo.png')} alt='Mia: Medical Information Assistant' /></Link>
   
 
   return (
