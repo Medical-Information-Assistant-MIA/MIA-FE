27316896cf2f359b2bb5e682efdf2f4c86dedeba
Sophie LaBelle <sophie.r.labelle@gmail.com>
2023-05-22 12:52:29 -0700

    Add conditional display for login and out buttons and style the login page

--- src/components/LoginPage/LoginPage.tsx
+++ src/components/LoginPage/LoginPage.tsx
@@ -22,16 +22,19 @@ export const LoginPage = ({setUserId}: UserID) =>  {
   }
 
   return (
-    <section className='nav-spacing login'>
-      <form onSubmit={ e => {
-        e.preventDefault()
-        checkLogin()
-      }}>
-        <input type='text' value={username} name='username' placeholder='username' required onChange={(e) => setUsername(e.target.value)} />
-        <input type='password' value={password} name='password' placeholder='password' required onChange={(e) => setPassword(e.target.value)} />
-        <button type='submit'>Login</button>
-      </form>
-      {error ? <p>Incorrect login, please try again.</p> : null}
-    </section>
+    <div className='nav-spacing login-page'>
+      <section className='login'>
+        <form onSubmit={ e => {
+          e.preventDefault()
+          checkLogin()
+        }}>
+          <h1>Please Log In</h1>
+          <input type='text' value={username} name='username' placeholder='username' required onChange={(e) => setUsername(e.target.value)} />
+          <input type='password' value={password} name='password' placeholder='password' required onChange={(e) => setPassword(e.target.value)} />
+          <button type='submit' className='submit-button'>Login</button>
+        </form>
+        {error ? <p>Incorrect login, please try again.</p> : null}
+      </section>
+    </div>
   )
 }
--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,12 +1,19 @@
+import { Link, useLocation } from 'react-router-dom';
 import './NavBar.css';
-import { Link } from 'react-router-dom';
 
 
 export const NavBar =  () => {
+  const location = useLocation();
+  const showLogin = location.pathname === '/' ? <button className='nav-btn go-back-btn'>Login</button> : <button className='nav-btn go-back-btn hidden'>Login</button>
+  const showLogOut = location.pathname !== '/' ? <button className='nav-btn go-back-btn'>Logout</button> : <button className='nav-btn go-back-btn hidden'>Logout</button>
+  const logoRoute = location.pathname !== '/' ? <Link to='/user-dashboard'><img className='logo' src={require('../../Mia-logo.png')} alt='Mia: Medical Information Assistant' /></Link> : <Link to='/'><img className='logo' src={require('../../Mia-logo.png')} alt='Mia: Medical Information Assistant' /></Link>
+  
+
   return (
     <nav className='nav-bar'>
-      <Link to='/'><img className='logo' src={require('../../Mia-logo.png')} alt='Mia: Medical Information Assistant' /></Link>
-      <Link to='/login'><button className='nav-btn'>Login</button></Link>
+      {logoRoute}
+      <Link to='/login'>{showLogin}</Link>
+      <Link to='/'>{showLogOut}</Link>
     </nav>
   );
 }
