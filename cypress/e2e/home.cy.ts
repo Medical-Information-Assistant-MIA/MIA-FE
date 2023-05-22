0693aa9faffb2bc6f91fe48d10673ff850e86b3c
Kass Daniello <zertroz@gmail.com>
2023-05-22 13:54:59 -0400

    Create utils and homepage tests

--- cypress/e2e/home.cy.ts
+++ cypress/e2e/home.cy.ts
@@ -1,5 +1,30 @@
+import { aliasQuery, aliasMutation } from "../utils/graphql-test-utils"
+
 describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
+  beforeEach(() => {
+    cy.intercept('POST', 'https://mia-be.herokuapp.com/graphql', (req) => {
+      aliasQuery(req, 'User')
+    })
+    .visit('http://localhost:3000/')
+  })
+
+  it('Should get a user', () => {
+    cy.wait('@gqlUserQuery')
+      .its('response.body.data.user')
+      .should('have.property', 'id')
+  })
+
+  it('Should land on the homepage', () => {
+    cy.get('h2').should('contain', 'Welcome to Mia')
+  })
+
+  it('Should navigate to the login page', () => {
+    cy.get('[href="/login"] > button').click()
+    .url().should('contain', '/login')
+  })
+
+  it('Should navigate to the dashboard', () => {
+    cy.get('.home-page > a > button').click()
+    .url().should('contain', '/user-dashboard')
   })
 })
