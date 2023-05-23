570dea3e6b9b076f805b940f620df6e6210d7a21
Amber Shipley <ashipley2@me.com>
2023-05-23 12:25:52 -0700

    Add error handling if doctor name is blank

--- src/components/NewConditionPage/DoctorForm/DoctorForm.tsx
+++ src/components/NewConditionPage/DoctorForm/DoctorForm.tsx
@@ -28,9 +28,12 @@ export const DoctorForm = ({conditionId}: NewDoctorProps) => {
           address: doctorInfo.address,
           category: doctorInfo.category
         }
-        await mutateFunction({
+        const data = await mutateFunction({
           variables: { input }
         });
+        if (data?.data?.createDoctor.errors.length) {
+          return;
+        }
       } catch(error) {
         return;
       }
@@ -44,7 +47,8 @@ export const DoctorForm = ({conditionId}: NewDoctorProps) => {
   }
 
   const [mutateFunction, {data, loading, error}] = useMutation(CREATE_DOCTOR);
-  
+  const mutateErrors = data?.createDoctor?.errors;
+
   return (
     <section className='condition-form'>
       <h2>Add a Doctor</h2>
@@ -58,9 +62,12 @@ export const DoctorForm = ({conditionId}: NewDoctorProps) => {
             address: doctorInfo.address,
             category: doctorInfo.category
           }
-          await mutateFunction({
+          const data = await mutateFunction({
             variables: { input }
           });
+          if (data?.data?.createDoctor.errors.length) {
+            return;
+          }
         } catch(error) {
           return;
         }
@@ -120,6 +127,7 @@ export const DoctorForm = ({conditionId}: NewDoctorProps) => {
       <button className='submit-button' type='button' onClick={handleClick} disabled={loading}>Go to health events</button>
       {loading ? <p>Loading...</p> : null}
       {error ? <p>Sorry, there was an error when submitting your form, please try again</p> : null}
+      {mutateErrors?.length ? <p>{mutateErrors}</p> : null}
       {success ? <p>Your doctor was successfully added, you can now add another one.</p> : null}
     </section>
   );
