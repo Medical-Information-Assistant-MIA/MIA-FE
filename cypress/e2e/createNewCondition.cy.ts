dfdb6933a22e5314cc97d721158c3f6b69039b60
Kass Daniello <zertroz@gmail.com>
2023-05-23 13:48:49 -0400

    Fix title form test stub

--- cypress/e2e/createNewCondition.cy.ts
+++ cypress/e2e/createNewCondition.cy.ts
@@ -3,10 +3,15 @@ import { aliasQuery, aliasMutation } from "../utils/graphql-test-utils"
 describe('template spec', () => {
   beforeEach(() => {
     cy.intercept('POST', 'https://mia-be.herokuapp.com/graphql', (req) => {
-      aliasQuery(req, 'User')
-      req.reply({
-        fixture: 'user-fixture.json'
-      })
+      if(req.body.operationName === 'User') {
+        aliasQuery(req, 'User')
+        req.reply({
+          fixture: 'user-fixture.json'
+        })
+      } else if (req.body.operationName === 'CreateCondition') {
+        aliasMutation(req, 'CreateCondition')
+      }
+      
     })
     .visit('http://localhost:3000/')
     cy.get('.submit-button').click()
@@ -19,9 +24,27 @@ describe('template spec', () => {
   it('Should add a new condition by title', () => {
     cy.get('input').type('Cold')
     .intercept('POST', 'https://mia-be.herokuapp.com/graphql', (req) => {
-      console.log(req.body)
-      aliasQuery(req, 'User')
-      aliasMutation(req, 'CreateCondition')
+      const { body } = req
+      console.log(req)
+      if(req.body.operationName === 'CreateCondition') {
+        req.alias = 'gqlCreateConditionMutation'
+        console.log('create condition')
+        req.reply({fixture: 'create-condition-response.json'})
+          // (res) => {
+          
+          // console.log('res', res)
+          // res.body = {
+          //   "data": {
+          //       "createCondition":{
+          //         "condition": {
+          //           "id": 4,
+          //           "name": "Cold"
+          //         }
+          //       }
+          //   }
+          // }
+        // })
+      }
     })
     
     cy.get('form > .submit-button').click()
