e8f2b549e50b0b7a8555afb9a12abd0848cd604c
Sophie LaBelle <sophie.r.labelle@gmail.com>
2023-05-24 09:40:41 -0700

    Add doctor display page, query and type

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -28,7 +28,7 @@ export const App = () => {
         <Route exact path='/' render={() => <HomePage />} />
         <Route exact path='/login' render={() => <LoginPage setUserId={setUserId} />} />
         <Route exact path='/user-dashboard' render={() => <UserDashboard user={data.user}/>} />
-        <Route exact path='/your-doctors' render={() => <DoctorPage user={data.user}/>}
+        <Route exact path='/your-doctors' render={() => <DoctorPage user={data.user}/>} />
         <Route exact path='/conditions/:id' render={({match}) => <ConditionPage key={match.params.id}/>} />
         <Route path='/add-condition' render={() => <NewConditionPage userId={userId} />} />
         <Route exact path='/404' render={() => <ErrorPage error={'Whoops, This Page does not exist'}/> } />
--- src/components/DoctorPage/DoctorPage.tsx
+++ src/components/DoctorPage/DoctorPage.tsx
@@ -1,22 +1,35 @@
-import { DoctorProps } from '../../types';
-import '/DoctorPage.css';
+import { useQuery } from '@apollo/client';
+import { Doctor, DoctorProps } from '../../types';
+import { GET_DOCTORS } from '../../gql-queries';
+import './DoctorPage.css';
 
 export const DoctorPage = ({user}: DoctorProps) => {
+  const id = user.id;
 
-  
+  const { loading, error, data } = useQuery(GET_DOCTORS, {
+    variables: { id }
+  });
+  if (loading) return <p className='nav-spacing doctor-page'>Loading...</p>
+  if (error) return <p className='nav-spacing doctor-page'>{error.message}</p>
 
-  // const docsDisp = doctors.map(doc => {
-  //   return (
-  //     <div className='doc-card'>
-
-  //     </div>
-  //   )
-  // })
+  console.log( data.userDoctors)
+  const docsDisp = data.userDoctors.map((doc: Doctor)=> {
+    return (
+      <div key={doc.id} className='doc-card'>
+        <h2>{doc.name}</h2>
+        <p>{doc.category}</p>
+        <p>{doc.phone}</p>
+        <p>{doc.address}</p>
+      </div>
+    )
+  })
 
   return (
-    <section className='nav-spacing'>
+    <section className='nav-spacing doctor-page'>
       <h1>Your Doctors</h1>
-      {docsDisp}
+      <div className='doc-card-conatiner'>
+        {docsDisp}
+      </div>
     </section>
   )
 }
