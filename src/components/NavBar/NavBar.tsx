cfed1452240428fd5ce3856994e1e2afd56bcf27
Sophie LaBelle <sophie.r.labelle@gmail.com>
2023-05-24 16:04:35 -0700

    Update turinary for nav buttons

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -4,11 +4,11 @@ import './NavBar.css';
 export const NavBar =  () => {
   const location = useLocation();
   const logoRoute = location.pathname !== '/login' && location.pathname !== '/' ? (<Link to='/user-dashboard'><img className='logo' src={require('../../Mia-logo.png')} alt='Mia: Medical Information Assistant' /></Link>) : (<Link to='/'><img className='logo' src={require('../../Mia-logo.png')} alt='Mia: Medical Information Assistant' /></Link>);
-  const showLogin = location.pathname === '/' ? (<button className='nav-btn'>Login</button>) : (<button className='hidden'>Login</button>);
-  const showLogOut = location.pathname !== '/' &&  location.pathname !== '/login' ? (<button className='nav-btn'>Logout</button>) : (<button className='hidden'>Logout</button>);
-  const showDashboard = location.pathname !== '/' && location.pathname !== '/login' && location.pathname !== '/user-dashboard' ? (<button className='nav-btn'>To Dashboard</button>) : (<button className='hidden'>Create New Condition</button>)
-  const showDoctors = location.pathname !== '/' && location.pathname !== '/your-doctors' && location.pathname !== '/login' ? (<button className='nav-btn'>Your Doctors</button>) : (<button className='hidden'>Your Doctors</button>);
-  const showAddCondition = location.pathname !== '/' && location.pathname !== '/add-condition' && location.pathname !== '/login' ? (<button className='nav-btn got-back-btn'>Create New Condition</button>) : (<button className='hidden'>Create New Condition</button>);
+  const showLogin = location.pathname === '/' ? (<button className='nav-btn'>Login</button>) : null;
+  const showLogOut = location.pathname !== '/' &&  location.pathname !== '/login' ? (<button className='nav-btn'>Logout</button>) : null;
+  const showDashboard = location.pathname !== '/' && location.pathname !== '/login' && location.pathname !== '/user-dashboard' ? (<button className='nav-btn'>Dashboard</button>) : null;
+  const showDoctors = location.pathname !== '/' && location.pathname !== '/your-doctors' && location.pathname !== '/login' ? (<button className='nav-btn'>Your Doctors</button>) : null;
+  const showAddCondition = location.pathname === '/user-dashboard' || location.pathname === '/your-doctors' ? (<button className='nav-btn got-back-btn'>Create New Condition</button>) : null;
 
   return (
     <nav className='nav-bar'>
