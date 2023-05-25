abbae9e2fbe7d18a61736f050365f38e651a469a
Sophie LaBelle <sophie.r.labelle@gmail.com>
2023-05-25 10:04:50 -0700

    Update Navigation routes to be more clean

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,22 +1,23 @@
-import { Link, Route, useLocation } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
+import { routes } from '../../routes/routes';
 import './NavBar.css';
 
 export const NavBar =  () => {
   const location = useLocation();
-  const logoRoute = location.pathname !== '/login' && location.pathname !== '/' ? (<Link to='/user-dashboard'><img className='logo' src={require('../../Mia-logo.png')} alt='Mia: Medical Information Assistant' /></Link>) : (<Link to='/'><img className='logo' src={require('../../Mia-logo.png')} alt='Mia: Medical Information Assistant' /></Link>);
-  const showLogin = location.pathname === '/';
-  const showLogOut = location.pathname !== '/' &&  location.pathname !== '/login';
-  const showDashboard = location.pathname !== '/' && location.pathname !== '/login' && location.pathname !== '/user-dashboard';
-  const showDoctors = location.pathname !== '/' && location.pathname !== '/your-doctors' && location.pathname !== '/login';
-  const showAddCondition = location.pathname === '/user-dashboard' || location.pathname === '/your-doctors';
+  const logoRoute = location.pathname !== routes.login && location.pathname !== routes.base ? (<Link to={routes.userDash}><img className='logo' src={require('../../Mia-logo.png')} alt='Mia: Medical Information Assistant' /></Link>) : (<Link to={routes.base}><img className='logo' src={require('../../Mia-logo.png')} alt='Mia: Medical Information Assistant' /></Link>);
+  const showLogin = location.pathname === routes.base;
+  const showLogOut = location.pathname !== (routes.base || routes.login);
+  const showDashboard = location.pathname !== (routes.base || routes.login || routes.userDash);
+  const showDoctors = location.pathname !== routes.base || routes.doctors || routes.login;
+  const showAddCondition = location.pathname === (routes.userDash || routes.doctors);
   
   return (
     <nav className='nav-bar'>
       {logoRoute}
-      {showLogin ? (<Link to='/login' className='nav-btn btn'>Login</Link>) : null}
-      {showLogOut ? (<Link to='/' className='nav-btn btn'>Logout</Link>) : null}
-      {showDashboard ? (<Link to='/user-dashboard' className='nav-btn btn'>Dashboard</Link>) : null}
-      {showDoctors ? (<Link to='/your-doctors' className='nav-btn btn'>Your Doctors</Link>) : null}
+      {showLogin ? (<Link to={routes.login} className='nav-btn btn'>Login</Link>) : null}
+      {showLogOut ? (<Link to={routes.base} className='nav-btn btn'>Logout</Link>) : null}
+      {showDashboard ? (<Link to={routes.userDash} className='nav-btn btn'>Dashboard</Link>) : null}
+      {showDoctors ? (<Link to={routes.doctors} className='nav-btn btn'>Your Doctors</Link>) : null}
       {showAddCondition ? (<Link to='/add-condition' className='nav-btn btn'>Create New Condition</Link>) : null}
     </nav>
   );
